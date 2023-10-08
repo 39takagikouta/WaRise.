033403b2eb167140a059a130e427e55291837a26
39takagikouta <kouta.takagi0728@gmail.com>
2023-10-08 17:17:02 +0900

    Add:タイムゾーンを日本に設定、デフォルトのローケルを日本に設定、日本語翻訳ファイルを作成

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,10 @@ module GraduationExam
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    
+    # タイムゾーンをTokyo（日本）にする
+    config.time_zone = "Tokyo"
+    # デフォルトのロケールを日本にする
+    config.i18n.default_locale = :ja
   end
 end
