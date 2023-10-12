be050bde80e35900d2375daa41bacb4f34c4a06d
39takagikouta <kouta.takagi0728@gmail.com>
2023-10-12 19:50:00 +0900

    Add:デバイスの遷移先を修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    mypage_path
+  end
+
+  def after_sign_up_path_for(resource)
+    mypage_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
 end
