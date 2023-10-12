f69400f287dcc416bbf6eaa6d7876824e9bcd042
39takagikouta <kouta.takagi0728@gmail.com>
2023-10-12 22:06:45 +0900

    Add:alarモデルに関する処理のroutingを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'mypage', to: 'alarms#mypage'
+  resources :alarms, only: %i[new create edit update destroy]
   devise_for :users
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
