a4c7c3f752a1724add71114f7f4017d9b1641ef7
39takagikouta <kouta.takagi0728@gmail.com>
2023-10-13 16:16:56 +0900

    Add:binding.pryを実行するためのgemを追加、ヘッダー「マイページ」のurlを修正

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -2,5 +2,6 @@ class MypagesController < ApplicationController
   def mypage
     @next_wake_up = Alarm.find_by(user_id: current_user.id, is_successful: nil)
     @alarms = Alarm.where(user_id: current_user.id)
+    binding.pry
   end
 end
