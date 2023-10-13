7bf32e8077d08baa595995c59fa1ba8dbca36768
39takagikouta <kouta.takagi0728@gmail.com>
2023-10-13 19:20:11 +0900

    Add:mypageを実装（途中まで）

--- app/controllers/alarms_controller.rb
+++ app/controllers/alarms_controller.rb
@@ -1,4 +1,9 @@
 class AlarmsController < ApplicationController
+  def mypage
+    @wake_up_time = Alarm.find_by(user_id: current_user.id, is_successful: nil)
+    @alarms = Alarm.where(user_id: current_user.id)
+  end
+
   def new
 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :alarms, only: %i[new create edit update destroy]
+  get 'mypage', to: 'alarms#mypage'
   devise_for :users
   root 'home#index'
-  get 'mypage', to: 'mypages#mypage'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
