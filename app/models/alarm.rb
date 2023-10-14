c2c57098f7cc9da62e13dc2657167c09f99f55ba
39takagikouta <kouta.takagi0728@gmail.com>
2023-10-14 17:41:49 +0900

    Fix:ActiveRecordの翻訳ファイルを作成し、カレンダーに起床予定時刻と起床結果を表示

--- app/models/alarm.rb
+++ app/models/alarm.rb
@@ -1,10 +1,7 @@
 class Alarm < ApplicationRecord
   belongs_to :user
 
-  def self.tomorrow_wake_up_for(user)
-    current_time = Time.zone.now
-    beginning_of_day = current_time.beginning_of_day
-    end_of_day = current_time.end_of_day
-    where(user: user).where('wake_up_time > ? AND wake_up_time <= ?', beginning_of_day, end_of_day + 1.day).first
+  def start_time
+    wake_up_time
   end
 end
