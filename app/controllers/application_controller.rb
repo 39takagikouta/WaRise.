7854f8c0909a016b4bc1adf622a8d3506badca02
39takagikouta <kouta.takagi0728@gmail.com>
2023-10-18 17:23:19 +0900

    Fix:ユーザー新規登録後の遷移先を嗜好性新規登録ページに設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    mypage_path
+    new_preference_path
   end
 
   def after_sign_up_path_for(resource)
