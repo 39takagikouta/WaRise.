fa45e84d9b9b42f32110e5b0c974e952bd194802
39takagikouta <kouta.takagi0728@gmail.com>
2023-10-29 01:44:29 +0900

    Add:LINEログインを途中まで実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: %i[line]
 
   has_many :alarms, dependent: :destroy
   has_many :user_comedy_tags, dependent: :destroy
