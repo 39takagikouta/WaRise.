a24d77a11cacbc8797a820c215f25d0527b45771
39takagikouta <kouta.takagi0728@gmail.com>
2023-12-03 04:41:31 +0900

    Add:webhookが使用できるか検証した

--- app/controllers/hoges_controller.rb
+++ app/controllers/hoges_controller.rb
@@ -1,5 +1,6 @@
 # app/controllers/hoges_controller.rb
 class HogesController < ApplicationController
+  skip_before_action :authenticate_user!
   protect_from_forgery except: :callback
 
   OMAJINAI = /アブラカタブラ|チチンプイプイ|ヒラケゴマ/
@@ -14,6 +15,24 @@ class HogesController < ApplicationController
     body = request.body.read
     signature = request.env['HTTP_X_LINE_SIGNATURE']
     return head :bad_request unless client.validate_signature(body, signature)
+
+    # もし「起床」ときたら
+      # もし所有者が「起床」を送ってきたユーザーであり、またwake_up_timeが現在時刻から10分以内であるalarmがあれば、
+        # もしitemに動画がなければ
+          # '申し訳ありません。設定していただいた検索ワードと動画の時間でレコメンドできる動画が無くなりました。検索ワードか時間、またはその両方を変更してください。'を表示する
+        # そうでなければ
+          # recommendアクションと同様の処理
+          # ののち動画を送信。
+      # そうでなければ
+        # 「現在のアラームがありません」の文言を送信
+    # もし「別の動画」ときたら
+      # 「別の動画」ときたらvieviodeos#createアクションの別の動画を表示する部分の処理を実行（動画の情報を引き継げるのか？）
+      # そして新しい動画を送信
+    # もし、「視聴完了」とメッセージが入っていたら
+      # vieviodeos#createアクションと同様の動作を行い、（動画の情報を引き継げるのか？）
+      # 保存完了をメッセージで送信する
+      # もし「視聴完了」の前に文章があれば
+        # それをコメントとして保存する
 
     events = client.parse_events_from(body)
     events.each do |event|
