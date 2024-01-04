d5b8a35c789d0e291e1c53a82dabc8cece5a5fd0
39takagikouta <kouta.takagi0728@gmail.com>
2024-01-04 19:23:44 +0900

    Rem:dumpファイルを削除

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,7 @@ Rails.application.routes.draw do
   resources :users, only: %i[show]
   get 'toggle_display', to: 'users#toggle_display'
   resources :preferences, only: %i[new create edit update]
-  resources :alarms, only: %i[new create edit update destroy index] do
-    get 'new_alarm_fields', on: :collection
-  end
+  resources :alarms, only: %i[new create edit update destroy index]
   get 'mypage', to: 'alarms#mypage'
   get 'recommend', to: 'alarms#recommend'
   get 'ranking', to: 'alarms#ranking'
