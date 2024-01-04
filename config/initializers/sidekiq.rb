4be0b87caa03fe90e80c5cb779cd3e065ee94049
39takagikouta <kouta.takagi0728@gmail.com>
2024-01-04 23:18:51 +0900

    Add:sidekiqの宛先を変更

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://127.0.0.1:6379' }
+  config.redis = ENV.fetch("REDIS_URL")
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://127.0.0.1:6379' }
+  config.redis = ENV.fetch("REDIS_URL")
 end
 
 require 'sidekiq/api'
