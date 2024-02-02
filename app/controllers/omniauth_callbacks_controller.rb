12e2672c94ab7bdc96607a4f9da3aa583785d30e
39takagikouta <kouta.takagi0728@gmail.com>
2024-02-03 03:36:43 +0900

    Fix:初ログインの場合に初期設定画面が表示されるよう処理を分岐。また、そのレイアウトに合わせてユーザー詳細ページと嗜好性編集ページを修正

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -7,6 +7,8 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @omniauth = request.env["omniauth.auth"]
     if @omniauth.present?
       @profile = User.find_or_initialize_by(provider: @omniauth["provider"], uid: @omniauth["uid"])
+      existing = false
+      existing = true if @profile.persisted?
       if @profile.email.blank?
         email = @omniauth["info"]["email"] || "#{@omniauth['uid']}-#{@omniauth['provider']}@example.com"
         @profile = current_user || User.create!(provider: @omniauth["provider"], uid: @omniauth["uid"], email:,
@@ -15,8 +17,14 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       @profile.set_values(@omniauth)
       sign_in(:user, @profile)
     end
-    flash[:notice] = "ログインしました"
-    redirect_to mypage_path
+
+    if existing
+      flash[:notice] = "ログインしました。"
+      redirect_to mypage_path
+    else
+      flash[:notice] = "新規登録が完了しました。"
+      redirect_to new_preference_path(current_user)
+    end
   end
 
   def fake_email(_uid, _provider)
