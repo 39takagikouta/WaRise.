07a2d3615d4f27acb663ed2a65ffb78eb63a33be
39takagikouta <kouta.takagi0728@gmail.com>
2024-02-27 18:51:32 +0900

    Add:「アラーム一覧」の文言が送られたら今日と明日のアラーム一覧が送られてくる機能を実装

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -37,7 +37,9 @@ class WebhooksController < ApplicationController
   end
 
   def reaction_text(event)
-    return "ユーザーが見つかりません。公式サイトからLINEログインをしてアカウントを登録してください。" unless User.find_by(uid: event['source']['userId'])
+    return "ユーザーが見つかりません。公式サイトからLINEログインをしてアカウントを登録してください。" unless user = User.find_by(uid: event['source']['userId'])
+
+    Alarm.set_false_to_is_successful(user)
 
     if event.message['text'].scan(/\d{8}/).any?
       alarm_strings = event.message['text'].split("\n")
@@ -58,13 +60,51 @@ class WebhooksController < ApplicationController
         "登録できませんでした。送信された数字が存在する時刻を表しているか確認してください。"
       else
         alarms.each do |alarm|
-          new_alarm = Alarm.create!(wake_up_time: alarm[:wake_up_time], custom_video_url: alarm[:custom_video_url], user_id: User.find_by(uid: event['source']['userId']).id)
+          binding.pry
+          new_alarm = Alarm.create!(wake_up_time: alarm[:wake_up_time], custom_video_url: alarm[:custom_video_url], user_id: user.id)
           job = SendNotificationJob.set(wait_until: new_alarm.wake_up_time).perform_later(new_alarm)
           new_alarm.update(job_id: job.provider_job_id)
-          message += "#{l alarm[:wake_up_time], format: :short} #{alarm[:custom_video_url].present? ? '動画あり' : '動画なし'}\n"
+          message += "#{l alarm[:wake_up_time], format: :short} #{alarm[:custom_video_url].present? ? '動画選択' : ''}\n"
         end
         message
       end
+    elsif event.message['text'] == "アラーム一覧"
+      message = "今日のアラーム\n"
+      binding.pry
+      today_alarms = Alarm.where(user_id: user.id, wake_up_time: Time.zone.now.all_day).order(:wake_up_time)
+      tomorrow_alarms = Alarm.where(user_id: user.id, wake_up_time: Time.zone.tomorrow.all_day).order(:wake_up_time)
+      if today_alarms.any?
+        today_alarms.each do |today_alarm|
+          is_successful = case today_alarm[:is_successful]
+                          when true
+                            '成功'
+                          when false
+                            '失敗'
+                          else
+                            ''
+                          end
+          message += "#{l today_alarm[:wake_up_time], format: :time} #{today_alarm[:custom_video_url].present? ? '動画選択' : ''} #{is_successful}\n"
+        end
+      else
+        message += "なし\n"
+      end
+      message += "\n明日のアラーム\n"
+      if tomorrow_alarms.any?
+        tomorrow_alarms.each do |tomorrow_alarm|
+          is_successful = case tomorrow_alarm[:is_successful]
+                          when true
+                            '成功'
+                          when false
+                            '失敗'
+                          else
+                            ''
+                          end
+          message += "#{l tomorrow_alarm[:wake_up_time], format: :time} #{tomorrow_alarm[:custom_video_url].present? ? '動画選択' : ''} #{is_successful}\n"
+        end
+      else
+        message += "なし\n"
+      end
+      message
     else
       "アラームの時刻や起床報告等の、決まった文言のみ受け取ることができます。"
     end
